f8bfc18dd07f6258c71a266d89aa7fdfa0febbe4
Matthew Holman <mateoholman@gmail.com>
2017-04-12 17:09:02 -0500

    Updated routing

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        React simple starter
+
+        //Render all child components of App after we render everything above
+        {this.props.children}
+      </div>
     );
   }
 }
