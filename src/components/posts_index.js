2508bbd80ac87626664990e9f91a7e9def964898
Matthew Holman <mateoholman@gmail.com>
2017-04-14 12:42:46 -0500

    Updated posts reducer

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
 
