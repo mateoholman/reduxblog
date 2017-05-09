76372ec0d43c51edccadc9928f60e4b8dbe89fb1
Matthew Holman <mateoholman@gmail.com>
2017-05-09 15:08:20 -0500

    Added the new posts button and route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -28,6 +28,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-sm btn-primary" to="/posts/new">
+            Add A Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
