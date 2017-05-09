9b0e981761de443347db4f9c0863190fe32b62ce
Matthew Holman <mateoholman@gmail.com>
2017-05-09 14:36:00 -0500

    Added the PostsNew component scaffolding

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        <h1>New Post!</h1>
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
