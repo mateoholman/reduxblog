d2ea8a115a9a21522a43831db933cd31ce332f50
Matthew Holman <mateoholman@gmail.com>
2017-05-15 16:38:52 -0500

    Wired the posts_new component to the action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,4 +11,13 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
 }
