299b327b6d64dc430e4e01b1ac8bb173a76b0839
Matthew Holman <mateoholman@gmail.com>
2017-05-15 18:03:26 -0500

    Added flow to the postsShow component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const post = this.props;
+
     return(
       <div>
+        <h3>{post.title}</h3>
+        <h6>{post.categories}</h6>
+        <p>{post.content}</h6>
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
