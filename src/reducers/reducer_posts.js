3c95fc558f3d5c45345ba6fe4187e6b967414809
Matthew Holman <mateoholman@gmail.com>
2017-05-15 17:34:02 -0500

    Created the getPost action creator & reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,16 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POST, FETCH_POSTS } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = {...state};
+      // newState[post.id] = post;
+      // return newState;
+      //With ES6 syntax, we are doing the same thing above with our return
+      //statement below!
+      return {...state, [action.payload.data.id]: action.payload.data}
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
